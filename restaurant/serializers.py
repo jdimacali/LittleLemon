d6ce736d77b33cde70069ba6f068d40247609b14
James Dimacali <frostbitezebra421@gmail.com>
2024-11-20 22:28:32 -0800

    added viewsets and generic routes for booking models

---
+++ restaurant/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+from .models import Menu, Booking
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Userfields = ["url", "username", "email", "groups"]
+
+
+class MenuItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Menu
+        fields = "__all__"
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
--- restaurant/urls.py
+++ restaurant/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
+router = DefaultRouter()
+router.register(r"booking", views.BookingViewSet)
+
 urlpatterns = [
-    path("", views.index, name="home"),
+    path("menu/", views.MenuItemView.as_view()),
+    path("menu/<int:pk>", views.SingleMenuItemView.as_view()),
+    path("", include(router.urls)),
 ]

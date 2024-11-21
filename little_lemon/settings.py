4404939603b7a4749cdb0e37b792255f710648f0
James Dimacali <frostbitezebra421@gmail.com>
2024-11-20 22:11:45 -0800

    added models for booking and menu and registered them

--- little_lemon/settings.py
+++ little_lemon/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = "little_lemon.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "LittleLemon",
+        "USER": "root",
+        "PASSWORD": "Shadowzebra421!",
+        "HOST": "localhost",
+        "OPTIONS": {"init_command": "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
 
--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} : {str(self.price)}"
+
+
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(max_length=6)
+    booking_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.name} for {self.no_of_guests} guests on {self.booking_date}"
